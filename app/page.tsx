63cce25aab47a961e04058c718fea1d674d6b441
Nazmul Haque <bpinazmul18@gmail.com>
2024-05-02 12:39:23 +0600

    feat: add about me

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar";
+import AboutMe from "@/components/AboutMe";
 
 export default function Home() {
   return (
       <main className="text-gray-400 bg-gray-900 body-font">
           <Navbar/>
+          <AboutMe/>
       </main>
   );
 }
