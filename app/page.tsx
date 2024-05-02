f1a2ea2a0c314ff68f9311171a112995bf3c6fb4
Nazmul Haque <bpinazmul18@gmail.com>
2024-05-02 13:15:22 +0600

    feat: add skills

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import AboutMe from "@/components/AboutMe";
 import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
           <Navbar/>
           <AboutMe/>
           <Projects/>
+          <Skills/>
       </main>
   );
 }
