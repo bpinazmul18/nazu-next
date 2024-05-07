69a5ec74bd4d2c0a74db4db683eeb8ecb47f30f5
Nazmul Haque <bpinazmul18@gmail.com>
2024-05-07 17:26:34 +0600

    remove: upwork profile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import NavbarLink from "@/components/common/NavbarLink";
 import AppLink from "@/components/common/AppLink";
 
-export default function Navbar () {
+export default function Navbar() {
     return (
         <header className="bg-gray-800 md:sticky top-0 z-10">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
@@ -19,7 +19,8 @@ export default function Navbar () {
                 </nav>
 
                 <AppLink
-                    destination="https://www.upwork.com/freelancers/~01b18bdb33d779bf10"
+                    // destination="https://www.upwork.com/freelancers/~01b18bdb33d779bf10"
+                    destination="#!"
                     classNames="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
                     target="_blank"
                 >
@@ -27,8 +28,8 @@ export default function Navbar () {
 
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                         <path fillRule="evenodd"
-                              d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
-                              clipRule="evenodd"/>
+                            d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
+                            clipRule="evenodd" />
                     </svg>
                 </AppLink>
             </div>
